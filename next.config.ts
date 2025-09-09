cd896ed2a54323130922de8fd920e30bb9f85cfa
devqasimgenetics <muhammad.qasim@genetics-pharmaceuticals.com>
2025-09-09 12:52:23 +0500

    fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  ignoreBuildErrors: true,
-};
+  eslint: {
+    ignoreDuringBuilds: true,
+  },};
 
 export default nextConfig;
