37487daec1a33e833bc5c60073b95b929835d195
Muhammad Qasim <muhammad.qasim@genetics-pharmaceuticals.com>
2025-11-04 12:08:54 +0500

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -57,7 +57,7 @@ const Footer = () => (
                         <h3 className="text-lg text-[#111111] font-bold mb-4">Stay Connected With Hablullah</h3>
                         <div className="space-y-3 text-gray-400">
                             <Link href={'https://www.youtube.com/@Hablullah-live'} target='_blank'>
-                                <p className="font-medium text-sm sm:text-base md:text-lg text-[#525252]">Podcast, videos and much more.</p>
+                                <p className="font-medium text-sm sm:text-base md:text-lg text-[#525252] translation-all hover:underline">Podcast, videos and much more.</p>
                             </Link>
                             <div className='text-xl flex items-center gap-2 sm:gap-3'>
                                 {/* <Link href={'https://play.google.com/store/apps/details?id=com.live.app.hablullah&hl=en&pli=1'} target='_blank'>
